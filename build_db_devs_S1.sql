4edbda61c01ece37fef8e7cecdba83df661415ec
Jerry Wang <jerry.wang@delphix.com>
2014-04-07 15:40:19 -0700

    add build_db_devs_S1.sql

---
+++ build_db_devs_S1.sql
@@ -0,0 +1,40 @@
+use master
+go
+
+sp_configure "number of devices", 100
+go
+
+disk init name = 'data_dev1',
+physname = '/opt/sybase/devices/data1/S1_data_dev1.dat',
+size = 512000
+go
+
+disk init name = 'data_dev2',
+physname = '/opt/sybase/devices/data1/S1_data_dev2.dat',
+size = 512000
+go
+
+disk init name = 'data_dev3',
+physname = '/opt/sybase/devices/data1/S1_data_dev3.dat',
+size = 512000
+go
+
+disk init name = 'data_dev4',
+physname = '/opt/sybase/devices/data1/S1_data_dev4.dat',
+size = 512000
+go
+
+disk init name = 'log_dev1',
+physname = '/opt/sybase/devices/log1/S1_log_dev1.dat',
+size = 512000
+go
+
+disk init name = 'log_dev2',
+physname = '/opt/sybase/devices/log1/S1_log_dev2.dat',
+size = 512000
+go
+
+disk init name = 'log_dev3',
+physname = '/opt/sybase/devices/log1/S1_log_dev3.dat',
+size = 512000
+go
