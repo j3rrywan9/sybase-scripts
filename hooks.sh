b1940283e0488447327b7aff24b94214d681ec9d
Jerry Wang <jerry.wang@delphix.com>
2015-09-22 10:36:41 -0700

    merge pre and post sync hooks

--- hooks.sh
+++ hooks.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+echo "Greetings from pre-sync script!"
+date >> /home/sybase/prepost_sync.txt
+echo hello from pre-sync >> /home/sybase/prepost_sync.txt
+
+#!/bin/sh
 echo "Greetings from post-sync script!"
 date >> /home/sybase/prepost_sync.txt
 echo hello from post-sync >> /home/sybase/prepost_sync.txt
--- pre_sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Greetings from pre-sync script!"
-date >> /home/sybase/prepost_sync.txt
-echo hello from pre-sync >> /home/sybase/prepost_sync.txt
-
